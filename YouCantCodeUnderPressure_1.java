938033889ff3c0dd607586e545550172bf65f871
domipaj <125757111+dxmi13@users.noreply.github.com>
2023-04-16 23:45:53 +0200

    YouCantCodeUnderPressure_1

---
+++ YouCantCodeUnderPressure_1.java
@@ -0,0 +1,8 @@
+//Code as fast as you can! You need to double the integer and return it.
+
+//My solution
+public class YouCantCodeUnderPressure_1 {
+    public static int doubleInteger(int i) {
+        return i*2;
+      }
+}
